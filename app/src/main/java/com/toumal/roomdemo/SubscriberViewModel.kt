65183a9b03c37edcafebd01df0b519e76871f5ab
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2020-10-15 04:58:16 +0600

    layout design done!

---
+++ app/src/main/java/com/toumal/roomdemo/SubscriberViewModel.kt
@@ -0,0 +1,8 @@
+package com.toumal.roomdemo
+
+import androidx.lifecycle.ViewModel
+import com.toumal.roomdemo.db.SubscriberRepository
+
+class SubscriberViewModel(private val repository: SubscriberRepository):ViewModel() {
+    
+}
